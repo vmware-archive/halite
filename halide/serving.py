ecaae0173a6dd6b4885134d4f828d3e3b858d059
Seth House <seth@eseth.com>
2013-07-08 19:32:18 -0600

    Removed trailing whitespace

--- halide/serving.py
+++ halide/serving.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python
 '''
 Runs wsgi web server using bottle framework
-    
+
 To get usage::
-    
+
     $ python serving.py -h
-    
+
 Runs embedded wsgi server when run directly as __main__.
 The server is at http://localhost:port or http://127.0.0.1:port
 The default port is 8080
 The root path is http://localhost:port
 and routes below are relative to this root path so
 "/" is http://localhost:port/
-     
+
 '''
 import sys
 import os
@@ -25,9 +25,9 @@ if __name__ == "__main__":
     '''
     Process command line args
     '''
-    
+
     levels = aiding.LOGGING_LEVELS #map of strings to logging levels
-    
+
     d = "Runs localhost wsgi service on given host address and port. "
     d += "\nDefault host:port is 0.0.0.0:8080."
     d += "\n(0.0.0.0 is any interface on localhost)"
@@ -37,24 +37,24 @@ if __name__ == "__main__":
                     default='info',
                     choices=levels.keys(),
                     help="Logging level.")
-    p.add_argument('-s','--server', 
+    p.add_argument('-s','--server',
                     action = 'store',
-                    nargs='?', 
-                    const='wsgiref', 
+                    nargs='?',
+                    const='wsgiref',
                     default='wsgiref',
                     help = "Wsgi server type.")
-    p.add_argument('-a','--host', 
+    p.add_argument('-a','--host',
                     action = 'store',
-                    nargs='?', 
-                    const='0.0.0.0', 
+                    nargs='?',
+                    const='0.0.0.0',
                     default='0.0.0.0',
                     help = "Wsgi server ip host address.")
-    p.add_argument('-p','--port', 
+    p.add_argument('-p','--port',
                     action = 'store',
-                    nargs='?', 
-                    const='8080', 
+                    nargs='?',
+                    const='8080',
                     default='8080',
-                    help = "Wsgi server ip port.")    
+                    help = "Wsgi server ip port.")
     p.add_argument('-r','--reload',
                     action = 'store_const',
                     const = True,
@@ -72,36 +72,36 @@ if __name__ == "__main__":
                     help = "Generate main.html dynamically.")
     p.add_argument('-c','--create',
                     action = 'store',
-                    nargs='?', 
+                    nargs='?',
                     const = 'app/main.html',
                     default = '',
                     help = "Create app/main.html (default) or given file and quit.")
     p.add_argument('-b','--base',
                     action = 'store',
-                    nargs='?', 
+                    nargs='?',
                     const = '/halide',
                     default = '/halide',
                     help = "Base Url for client side web application.")
     p.add_argument('-C','--coffee',
                     action = 'store',
-                    nargs='?', 
+                    nargs='?',
                     const = True,
                     default = False,
-                    help = "When -g or -c configure main.html to compile coffeescript.")    
+                    help = "When -g or -c configure main.html to compile coffeescript.")
 
     args = p.parse_args()
-    
+
     logger = aiding.getLogger(name="Halide", level=levels[args.level])
-    
+
     if args.create:
         logger.info("Creating %s" % args.create)
         path = os.path.abspath(args.create)
         import ending
         ending.logger.setLevel(levels[args.level]) # set bottle app logger from args
-        ending.development = args.devel         
+        ending.development = args.devel
         ending.createStaticMain(path=path, base=args.base)
         sys.exit()
-    
+
     if args.server in ['gevent']:
         try:
             import gevent
@@ -109,35 +109,35 @@ if __name__ == "__main__":
             monkey.patch_all()
         except ImportError as ex: #gevent support not available
             args.server = 'wsgiref' # use default server
-    
-    
+
+
     import bottle
-    
-    
+
+
     logger.info("Running web application with server %s on %s:%s" %
                 (args.server, args.host, args.port))
-    
+
     if args.devel:
         logger.info("Running in development mode")
     logger.info("Logger %s at level %s." % (logger.name, args.level))
-    
+
     #inject dependencies before using app
     import ending  #bottle app file
-    
+
     ending.logger.setLevel(levels[args.level]) # set bottle app logger from args
-    
-    ending.development = args.devel 
+
+    ending.development = args.devel
     ending.generate = args.gen
     ending.baseprefix = args.base
     ending.coffeescript = args.coffee
     ending.remount(base=args.base)
     from ending import app
-     
+
     bottle.run( app=app,
                 server=args.server,
                 host=args.host,
                 port=args.port,
-                debug=args.devel, 
-                reloader=args.reload, 
+                debug=args.devel,
+                reloader=args.reload,
                 interval=1,
                 quiet=False)
