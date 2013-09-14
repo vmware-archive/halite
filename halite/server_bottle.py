abd63c3b41c163152bec34819cef9156ae3a5f26
David Boucha <boucha@gmail.com>
2013-09-14 11:40:48 -0600

    remove whitespace

--- halite/server_bottle.py
+++ halite/server_bottle.py
@@ -1,18 +1,18 @@
 #!/usr/local/bin/python2.7
 
 ''' Runs wsgi web server using bottle framework
-    
+
     To get usage
-    
+
     $ python server_bottle.py -h
-    
+
     Runs embedded wsgi server when run directly as __main__.
     The server is at http://localhost:port or http://127.0.0.1:port
     The default port is 8080
     The root path is http://localhost:port
     and routes below are relative to this root path so
     "/" is http://localhost:port/
-     
+
 '''
 import sys
 import os
@@ -42,7 +42,7 @@ MOLD_DIR_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'mold')
 
 def loadWebUI(app, devel=False, coffee=False):
     ''' Load endpoints for bottle app'''
-        
+
     #catch all for page refreshes of any app url
     @app.route('/app/<path:path>') # /app/<path>
     @app.route('/app/') # /app/
@@ -56,20 +56,20 @@ def loadWebUI(app, devel=False, coffee=False):
                                             devel=args.devel,
                                             save=False,))
         return bottle.static_file('main.html', root=STATIC_APP_PATH)
-    
+
     @app.route('/static/lib/<filepath:re:.*\.(woff)>')
     def staticFontWoffGet(filepath):
-        return bottle.static_file(filepath, root=STATIC_LIB_PATH, mimetype='application/font-woff')    
-        
+        return bottle.static_file(filepath, root=STATIC_LIB_PATH, mimetype='application/font-woff')
+
     @app.route('/static/app/<filepath:path>')
     def staticAppGet(filepath):
         return bottle.static_file(filepath, root=STATIC_APP_PATH)
-    
+
     @app.route('/static/lib/<filepath:path>')
     def staticLibGet(filepath):
         return bottle.static_file(filepath, root=STATIC_LIB_PATH)
-    
-    @app.get('/test') 
+
+    @app.get('/test')
     def testGet():
         '''
         Test endpoint for bottle application
@@ -79,20 +79,20 @@ def loadWebUI(app, devel=False, coffee=False):
         bottle.response.set_header('content-type', 'text/plain')
         content =  "Web app file is located at %s" % os.path.dirname(os.path.abspath(__file__))
         siteMap = ""
-        
+
         currentApp = bottle.app()
-        
+
         for route in currentApp.routes:
             siteMap = "%s%s%s %s" %  (siteMap, '\n' if siteMap else '', route.rule, route.method)
             target = route.config.get('mountpoint', {}).get('target')
             if target:
                 for way in target.routes:
                     siteMap = "%s\n    %s %s" %  (siteMap, way.rule, way.method)
-                    
+
         content = "%s\n%s" %  (content, siteMap)
         return content
-    
-    
+
+
     @app.get('/echo')
     @app.get('/echo/<action>')
     def echoGet(action=None):
@@ -102,16 +102,16 @@ def loadWebUI(app, devel=False, coffee=False):
         '''
         #convert to json serializible dict
         query = { key: val for key, val in bottle.request.query.items()}
-        
+
         data = dict(verb='GET',
                     url=bottle.request.url,
                     action=action,
                     query=query,
                     content=bottle.request.json)
-    
+
         return data
-    
-    @app.get('/ping') 
+
+    @app.get('/ping')
     def pingGet():
         ''' Send salt ping'''
         import salt.client
@@ -120,9 +120,9 @@ def loadWebUI(app, devel=False, coffee=False):
                     os.environ.get('SALT_MASTER_CONFIG', '/etc/salt/master'))
         local = salt.client.LocalClient(__opts__['conf_file'])
         local.cmd('*', 'test.ping',  username="saltwui", password='dissolve', eauth='pam')
-        
+
         return dict(result = "Sent Ping")
-    
+
     @app.get('/stream')
     def streamGet():
         ''' Create server sent event stream with counter'''
@@ -137,9 +137,9 @@ def loadWebUI(app, devel=False, coffee=False):
             yield 'data: %i\n\n' % n
             n += 1
             gevent.sleep(1.0) if gevented else time.sleep(1.0)
-            
+
         yield "data: END\n\n"
-        
+
 def tokenify(cmd, token=None):
     '''
     If token is not None Then assign token to 'token' key of dict cmd and return cmd
@@ -153,30 +153,30 @@ def loadSaltApi(app):
     ''' Load endpoints for Salt-API '''
     from salt.exceptions import EauthAuthenticationError
     import salt.client.api
-    
+
     sleep = gevent.sleep if gevented else time.sleep
-    
-    @app.post('/login') 
+
+    @app.post('/login')
     def loginPost():
         ''' Login and respond with login credentials'''
         data = bottle.request.json
         if not data:
-            bottle.abort(400, "Login data missing.")        
-        
+            bottle.abort(400, "Login data missing.")
+
         creds = dict(username=data.get("username"),
                      password=data.get("password"),
                      eauth=data.get("eauth"))
-        
+
         client = salt.client.api.APIClient()
         try:
             creds = client.create_token(creds)
         except EauthAuthenticationError as ex:
             bottle.abort(401, text=repr(ex))
-            
+
         bottle.response.set_header('X-Auth-Token', creds['token'])
-        return {"return": [creds]} 
-    
-    @app.post('/logout') 
+        return {"return": [creds]}
+
+    @app.post('/logout')
     def logoutPost():
         '''
         Logout
@@ -188,7 +188,7 @@ def loadSaltApi(app):
         else:
             result = {}
         return result
-    
+
     @app.post(['/signature', '/signature/<token>'])
     def signaturePost(token = None):
         '''
@@ -197,24 +197,24 @@ def loadSaltApi(app):
         '''
         if not token:
             token = bottle.request.get_header('X-Auth-Token')
-        
+
         cmds = bottle.request.json
         if not cmds:
             bottle.abort(code=400, text='Missing command(s).')
-            
+
         if hasattr(cmds, 'get'): #convert to array
             cmds =  [cmds]
-        
+
         client = salt.client.api.APIClient()
         try:
             results = [client.signature(tokenify(cmd, token)) for cmd in cmds]
         except EauthAuthenticationError as ex:
             bottle.abort(code=401, text=repr(ex))
         except Exception as ex:
-            bottle.abort(code=400, text=repr(ex))            
-            
+            bottle.abort(code=400, text=repr(ex))
+
         return {"return": results}
-    
+
     @app.post(['/run', '/run/<token>'])
     def runPost(token = None):
         '''
@@ -223,24 +223,24 @@ def loadSaltApi(app):
         '''
         if not token:
             token = bottle.request.get_header('X-Auth-Token')
-        
+
         cmds = bottle.request.json
         if not cmds:
             bottle.abort(code=400, text='Missing command(s).')
-            
+
         if hasattr(cmds, 'get'): #convert to array
             cmds =  [cmds]
-        
+
         client = salt.client.api.APIClient()
         try:
             results = [client.run(tokenify(cmd, token)) for cmd in cmds]
         except EauthAuthenticationError as ex:
             bottle.abort(code=401, text=repr(ex))
         except Exception as ex:
-            bottle.abort(code=400, text=repr(ex))            
-            
-        return {"return": results}   
-        
+            bottle.abort(code=400, text=repr(ex))
+
+        return {"return": results}
+
     @app.get('/event/<token>')
     def eventGet(token):
         '''
@@ -251,27 +251,27 @@ def loadSaltApi(app):
         '''
         if not token:
             bottle.abort(401, "Missing token.")
-        
+
         client = salt.client.api.APIClient()
-        
+
         if not client.verify_token(token): #auth.get_tok(token):
             bottle.abort(401, "Invalid token.")
-            
+
         tag = bottle.request.query.get('tag', '')
-        
+
         bottle.response.set_header('Content-Type',  'text/event-stream') #text
         bottle.response.set_header('Cache-Control',  'no-cache')
-    
+
         # Set client-side auto-reconnect timeout, ms.
         yield 'retry: 250\n\n'
-    
+
         while True:
             data =  client.get_event(wait=0.025, tag=tag, full=True)
             if data:
                 yield 'data: {0}\n\n'.format(json.dumps(data))
             else:
                 sleep(0.1)
-             
+
     @app.post(['/fire', '/fire/<token>'])
     def firePost(token=None):
         '''
@@ -284,29 +284,29 @@ def loadSaltApi(app):
         Post body is either list of events or single event
         '''
         if not token:
-            token = bottle.request.get_header('X-Auth-Token')        
+            token = bottle.request.get_header('X-Auth-Token')
         if not token:
             bottle.abort(401, "Missing token.")
-        
+
         client = salt.client.api.APIClient()
-        
+
         if not client.verify_token(token): #auth.get_tok(token):
             bottle.abort(401, "Invalid token.")
-        
+
         events = bottle.request.json
         if not events:
             bottle.abort(code=400, text='Missing event(s).')
-        
+
         if hasattr(events, 'get'): #convert to list if not
             events = [events]
-        
+
         results = [dict(tag=event['tag'],
                         result=client.fire_event(event['data'], event['tag']))
                    for event in events]
-        
+
         bottle.response.set_header('Content-Type',  'application/json')
         return json.dumps(results)
-        
+
     return app
 
 def loadCors(app):
@@ -314,12 +314,12 @@ def loadCors(app):
     Load support for CORS Cross Origin Resource Sharing
     '''
     corsRoutes = ['/login', '/logout',
-                  '/signature', '/signature/<token>', 
+                  '/signature', '/signature/<token>',
                   '/run', 'run/<token>',
                   '/event/<token>',
                   '/fire', '/fire/<token>'
                   ]
-    
+
     @app.hook('after_request')
     def enableCors():
         '''
@@ -333,14 +333,14 @@ def loadCors(app):
                             'PUT, GET, POST, DELETE, OPTIONS')
         bottle.response.set_header('Access-Control-Allow-Headers', 
             'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token, X-Auth-Token')
-    
+
     @app.route(corsRoutes, method='OPTIONS')
     def allowOption(path=None):
         '''
         Respond to OPTION request method
         '''
         return {}
-    
+
     return app
 
 def loadErrors(app):
@@ -353,27 +353,27 @@ def loadErrors(app):
     def error400(ex):
         bottle.response.set_header('content-type', 'application/json')
         return json.dumps(dict(error=ex.body))
-    
+
     @app.error(401)
     def error401(ex):
         bottle.response.set_header('content-type', 'application/json')
-        return json.dumps(dict(error=ex.body))    
-    
+        return json.dumps(dict(error=ex.body))
+
     @app.error(404)
     def error404(ex):
         ''' Use json 404 if request accepts json otherwise use html'''
         if 'application/json' not in bottle.request.get_header('Accept', ""):
             bottle.response.set_header('content-type', 'text/html')
             return bottle.tonat(bottle.template(bottle.ERROR_PAGE_TEMPLATE, e=ex))
-        
-        bottle.response.set_header('content-type', 'application/json')    
+
+        bottle.response.set_header('content-type', 'application/json')
         return json.dumps(dict(error=ex.body))
-    
+
     @app.error(405)
     def error405(ex):
         bottle.response.set_header('content-type', 'application/json')
         return json.dumps(dict(error=ex.body))
-    
+
     @app.error(409)
     def error409(ex):
         bottle.response.set_header('content-type', 'application/json')
@@ -395,7 +395,7 @@ def rebase(base):
     ''' Create new app using current app routes prefixed with base'''
     if not base: #no rebase needed
         return bottle.app()
-    
+
     oldApp = bottle.app.pop()
     newApp = bottle.app.push()
     for route in oldApp.routes:
@@ -404,45 +404,45 @@ def rebase(base):
         route.reset() #reapply plugins on next call
     return newApp
 
-    
-def createStaticMain(kind='bottle', 
+
+def createStaticMain(kind='bottle',
                      moldPath=MOLD_DIR_PATH,
                      base="",
                      devel=False,
                      coffee=False,
-                     save=False, 
+                     save=False,
                      path=os.path.join(STATIC_APP_PATH, 'main.html')):
     """
     Generate and return main.html using template filepath mold and optionally
     write to filepath path is save
-    
+
     """
     data = dict(baseUrl=base,
                 mini=".min" if not devel else "",
                 coffee = coffee )
-       
+
     #get lists of app scripts and styles filenames
     scripts, sheets = aiding.getFiles( top=STATIC_APP_PATH,
                                        prefix='',
                                        coffee = coffee)
     data['scripts'] = scripts
     data['sheets'] = sheets
-    
+
     content =  ''
     mainMoldPath = os.path.join(moldPath, 'main_{0}.html'.format(kind))
     with open(mainMoldPath, "ru") as fp:
-        mold = fp.read()                                
+        mold = fp.read()
         if kind == 'bottle':
             content = bottle.SimpleTemplate(mold).render(**data)
         #elif kind == 'mustache':
             #content = staching.render(mold, data)
-    
+
     if content and save:
         with open(path, 'w+') as fp:
             fp.write(content)
-    
+
     return content
-    
+
 
 def startServer(level='info',
                 server='paste',
@@ -478,12 +478,12 @@ def startServer(level='info',
                    default is False
         coffee = generate main.html to load and compile coffeescript if truthy when
                    devel is true. Default is False
-        kwas = additional keyword arguments dict that are passed as server options           
+        kwas = additional keyword arguments dict that are passed as server options
     Does not return.
     '''
     #so when using gevent can monkey patch before import bottle
-    global gevented, gevent, bottle 
-    
+    global gevented, gevent, bottle
+
     logger.setLevel(aiding.LOGGING_LEVELS[level])
     gevented = False
     if server in ['gevent']:
@@ -494,57 +494,57 @@ def startServer(level='info',
             gevented = True
         except ImportError as ex: #gevent support not available
             args.server = 'paste' # use default server
-    
+
     tlsOptions = {}
     tlsOptions['paste'] = {'ssl_pem': pempath}
     tlsOptions['gevent'] = {'keyfile': keypath, 'certfile': certpath}
     tlsOptions['cherrypy'] = {'keyfile': keypath, 'certfile': certpath}
-    
+
     options = dict(**kwas)
     if tls and args.server in tlsOptions:
         options.update(**tlsOptions[args.server]) # retrieve ssl options for server
-    
+
     import bottle
-    
+
     app = bottle.default_app() # create bottle app
-    
+
     loadErrors(app)
     loadWebUI(app, devel=devel, coffee=coffee)
     loadSaltApi(app)
     if cors:
         loadCors(app)
     app = rebase(base=base)
-    
+
     logger.info("Running web application server '{0}' on {1}:{2}.".format(
         server, host, port))
-    
+
     if base:
         logger.info("URL paths rebased after base '{0}".format(base))
     logger.info("CORS is {0}.".format('enabled' if cors else 'disabled'))
     logger.info("TLS/SSL is {0}.".format('enabled' if tls else 'disabled'))
     logger.info("Server options: \n{0}".format(options))
-     
+
     bottle.run( app=app,
                 server=server,
                 host=host,
                 port=port,
-                debug=True, 
-                reloader=False, 
+                debug=True,
+                reloader=False,
                 interval=1,
                 quiet=False,
-                **options)    
+                **options)
 
 
 def parseArgs():
     '''
     Process command line args using argparse or if not available the optparse
        in a backwards compatible way
-       
+
     Returns tuple of (args, remnants) where args is object with attributes
        corresponding to named arguments and remnants is list of remaining
        unnamed positional arguments
     '''
-    
+
     try: # make backwards compatible with deprecated optparse
         from argparse import ArgumentParser as Parser
         Parser.add = Parser.add_argument
@@ -555,7 +555,7 @@ def parseArgs():
         Parser.add =  Parser.add_option
         Parser.add_group = Parser.add_option_group
         Parser.parse =  Parser.parse_args
-    
+
     d = "Runs localhost web application wsgi service on given host address and port. "
     d += "\nDefault host:port is 0.0.0.0:8080."
     d += "\n(0.0.0.0 is any interface on localhost)"
@@ -565,18 +565,18 @@ def parseArgs():
             default='info',
             choices=aiding.LOGGING_LEVELS.keys(),
             help="Logging level.")
-    p.add('-s','--server', 
+    p.add('-s','--server',
             action = 'store',
             default='paste',
             help = "Web application WSGI server type.")
-    p.add('-a','--host', 
+    p.add('-a','--host',
             action = 'store',
             default='0.0.0.0',
             help = "Web application WSGI server ip host address.")
-    p.add('-p','--port', 
+    p.add('-p','--port',
             action = 'store',
             default='8080',
-            help = "Web application WSGI server ip port.")    
+            help = "Web application WSGI server ip port.")
     p.add('-b','--base',
             action = 'store',
             default = '',
@@ -584,7 +584,7 @@ def parseArgs():
     p.add('-x','--cors',
             action = 'store_true',
             default = False,
-            help = "Enable CORS Cross Origin Resource Sharing on server.")    
+            help = "Enable CORS Cross Origin Resource Sharing on server.")
     p.add('-t','--tls',
             action = 'store_true',
             default = False,
@@ -617,36 +617,36 @@ def parseArgs():
     p.add('-d','--devel',
                 action = 'store_true',
                 default = False,
-                help = "Development mode.")    
-    
-    
-    return (p.parse())   
+                help = "Development mode.")
+
+
+    return (p.parse())
 
 
 if __name__ == "__main__":
     '''
     Processes command line arguments and then runs web application server
-    
+
     Invoke with '-h' command line option to get usage string
     '''
     args, remnants = parseArgs()
     logger.setLevel(aiding.LOGGING_LEVELS[args.level]) # set bottle app logger from args
-    
+
     if args.gen:
         import bottle
         logger.info("Generating %s from template only." % args.load)
-        createStaticMain(   kind='bottle', 
+        createStaticMain(   kind='bottle',
                             base=args.base,
                             devel=args.devel,
                             coffee=args.coffee,
-                            save=True, 
+                            save=True,
                             path=os.path.abspath(args.load))
-        sys.exit()    
-    
+        sys.exit()
+
     startServer(level=args.level,
                 server=args.server,
                 host=args.host,
-                port=args.port, 
+                port=args.port,
                 base=args.base,
                 cors=args.cors,
                 tls=args.tls,
@@ -654,5 +654,5 @@ if __name__ == "__main__":
                 keypath=args.key,
                 pempath=args.pem,
                 devel=args.devel,
-                coffee=args.coffee, 
+                coffee=args.coffee,
                  )
