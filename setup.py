34ad8df8dee301a862cdd358a3562c1b809a7471
Samuel M Smith <smith.samuel.m@gmail.com>
2013-09-16 18:32:24 -0600

    bumped version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'halite',
-    version = '0.0.7', 
+    version = '0.0.8', 
     description = 'SaltStack Web UI',
     url = 'https://github.com/saltstack/halite',
     author='SaltStack Inc',
