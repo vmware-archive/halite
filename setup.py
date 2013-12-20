1a110307c15694d1e60bbbb073a9ee41104b804a
Aditya Kulkarni <adi@saltstack.com>
2013-12-20 16:55:26 -0500

    Fixes and release

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'halite',
-    version = '0.1.14',
+    version = '0.1.15',
     description = 'SaltStack Web UI',
     url = 'https://github.com/saltstack/halite',
     author='SaltStack Inc',
